dd0ac8c5bbcb4a0db76375d6329e971ee7c38360
mpitcel <3997366+mpitcel@users.noreply.github.com>
2023-10-23 11:49:05 -0500

    Add debug output of Git Hash

--- src/phtoolbox/app/base_connector.py
+++ src/phtoolbox/app/base_connector.py
@@ -21,6 +21,10 @@ class NiceBaseConnector(BaseConnector):
     def __init__(self):
         super(NiceBaseConnector, self).__init__()
 
+        self.__version__ = 'UNSET_VERSION'
+        self.__git_hash__ = 'UNSET_GIT_HASH'
+        self.__build_time__ = 'UNSET_BUILD_TIME'
+	    
         self.actions = {}
         for _, method in inspect.getmembers(self):
             if hasattr(method, '_handle'):
@@ -31,13 +35,24 @@ class NiceBaseConnector(BaseConnector):
 
     def handle_action(self, param):
         action_id = self.get_action_identifier()
-
+	self.debug_version_info()
         self.debug_print("action_id", self.get_action_identifier())
 
         if action_id in self.actions.keys():
             return self.actions[action_id](param)
 
         return phantom.APP_ERROR
+
+    def debug_version_info(self):
+        """ Developers are encouraged to set the following values:
+            - self.__version__ = 'GITHUB_TAG'
+            - self.__git_hash__ = 'GITHUB_SHA'
+            - self.__build_time__ = 'BUILD_TIME'
+        """
+
+        self.debug_print("Version: " + self.__version__)
+        self.debug_print("Git Hash: " + self.__git_hash__)
+        self.debug_print("Build Time:" + self.__build_time__)
 
     def initialize(self):
 	    # Load the state in initialize, use it to store data
