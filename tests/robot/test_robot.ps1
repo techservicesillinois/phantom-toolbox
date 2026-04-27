a975a68bfdfb975abb32cde3e7b7828c8808b4d6
Edward <delaport@illinois.edu>
2026-04-27 16:05:44 -0500

    Fixup

--- tests/robot/test_robot.ps1
+++ tests/robot/test_robot.ps1
@@ -1,8 +1,8 @@
 #!/usr/bin/env pwsh
-$env:PATH=".tox/wheel/Scripts:$env:PATH" 
+$ErrorActionPreference = "Stop"
+$env:PATH="$(Get-Location)/.tox/wheel/Scripts:$env:PATH" 
 Write-Output "Path is " + $env:PATH
 which phantom
 phantom --help
 Write-Output "************* End of Debug for Win Accept Test ***********************"
 robot tests/robot
-Write-Output "************* End of Win Accept Test ***********************"
