06ecd8d9507ee3a878242b95138d1c1fa116b6fd
Edward Delaporte <edward@delaporte.us>
2026-04-28 14:23:44 -0500

    Fixup (#41)
    * Fixup
    
    * Fixup
    
    * Add CODEOWNDERS

--- tests/robot/test_robot.ps1
+++ tests/robot/test_robot.ps1
@@ -1,9 +1,4 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference = "Stop"
-ls -r "$(Get-Location)\.tox\wheel\Scripts"
-$env:PATH="$(Get-Location)\.tox\wheel\Scripts;$env:PATH" 
-Write-Output "Path is " + $env:PATH
-Get-Command phantom
-phantom --help
-Write-Output "************* End of Debug for Win Accept Test ***********************"
+$env:PATH="$(Get-Location)\.tox\wheel\Scripts;$env:PATH"
 robot tests/robot
